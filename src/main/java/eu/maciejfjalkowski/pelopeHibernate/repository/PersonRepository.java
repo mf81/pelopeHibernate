d6837576dcbc8b458f1f1def9401019d3e972047
mf81 <maciej.fijalkowski81@gmail.com>
2018-02-16 12:37:20 +0100

    add singel view for people

--- src/main/java/eu/maciejfjalkowski/pelopeHibernate/repository/PersonRepository.java
+++ src/main/java/eu/maciejfjalkowski/pelopeHibernate/repository/PersonRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface PersonRepository extends CrudRepository<Person,Long>{
-
     List<Person> findByNameIgnoreCase(String name);
     List<Person> findBySurnameIgnoreCase(String surname);
 }
